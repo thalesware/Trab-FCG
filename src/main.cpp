14e7c14342db269f72bb426cf379faa0a366724b
Thales <thales.alcantara.rocha@gmail.com>
2021-04-01 09:08:42 -0300

    Camera variation by selected tile Attempt 1
    attempting to change camera angle based on wich tile is selected

--- src/main.cpp
+++ src/main.cpp
@@ -717,10 +717,14 @@ int main(int argc, char* argv[])
         float z = r*cos(g_CameraPhi)*cos(g_CameraTheta);
         float x = r*cos(g_CameraPhi)*sin(g_CameraTheta);
 
+        float x1 = stage1.tilesArray[selectedTile].origin_shift_x;
+        float y1 = stage1.tilesArray[selectedTile].origin_shift_y;
+        float z1 = stage1.tilesArray[selectedTile].origin_shift_z;
+
         // Abaixo definimos as varáveis que efetivamente definem a câmera virtual.
         // Veja slides 195-227 e 229-234 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
         glm::vec4 camera_position_c  = glm::vec4(x,y,z,1.0f); // Ponto "c", centro da câmera
-        glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
+        glm::vec4 camera_lookat_l    = glm::vec4(x1,y1,z1,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
         glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
         glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
 
